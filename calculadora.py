ff38242baf53d0ec0ade81608d2c5d0da4aee606
thaliadani <thaliadani2@gmail.com>
2025-02-11 15:26:30 -0300

    modified:   calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -10,7 +10,7 @@ def calculadora(num1, num2, op):
     else:
         return 0
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
 op = input("Digite a operação (+, -, *, /): ")
 print(calculadora(num1, num2, op))
